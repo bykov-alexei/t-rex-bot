b18bb24b95cb528d3875a2afd9202f538fb50f56
Bykov Alexei <bitskoff@gmail.com>
2020-10-28 11:24:06 +0800

    added-some-field-state-functions

--- recognizer.py
+++ recognizer.py
@@ -1,5 +1,13 @@
-from skimage.measure import label, regionprops
+import argparse
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--game-over', action='store_true', dest='game_over')
+parser.add_argument('--file', dest='file', required=True)
+args = parser.parse_args()
+
+from skimage.measure import label, regionprops
+from skimage.transform import resize
+import numpy as np
 import matplotlib.pyplot as plt
 
 
@@ -18,13 +26,41 @@ def binarize(screen, threshold):
 
 
 def find_field(screen, threshold=lambda x: 0.95, find_field=field_by_size):
+    # Fixing fields that are black on background
+    b = binarize(screen, threshold=threshold)
+    if (np.sum(b == 0) // np.sum(b == 1) > 2):
+        screen = 1 - screen
+    
     b = binarize(screen, threshold=threshold)
     field_bbox = find_field(b)
     field = screen[field_bbox[0]:field_bbox[2], field_bbox[1]:field_bbox[3]]
     return field
+
+def vertical_lines(image):
+    lines = np.sum(image.sum(axis=0) / image.shape[0] == 1)
+    return lines
+
+def horizontal_lines(image):
+    lines = np.sum(image.sum(axis=1) / image.shape[1] == 1)
+    return lines
+
+def is_game_over(field, threshold=lambda x: 0.75):
+    b = binarize(field, threshold)
+    regions = regionprops(label(~b))
+    for region in regions:
+        image = resize(region.image, (10, 10))
+        vertical = vertical_lines(image)
+        horizontal = horizontal_lines(image)
+        if vertical == 3 and horizontal == 4:
+            return True
+    return False
     
 
 if __name__ == '__main__':
-    screen = plt.imread('screen.png')
-    gr = find_field(screen)
-    plt.imsave('gr.png', gr, cmap='gray')
+    filename = args.file
+    if args.game_over:
+        screen = plt.imread(filename)
+        field = find_field(screen)
+        game_over = is_game_over(field)
+        print("Game is over" if game_over else "Game isn't over")
+        plt.imsave('result.png', field, cmap='gray')
